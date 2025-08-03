05cf8bd57343303a7c065b70294fcde4f5d346ce
mahnishant <141440558+mahnishant@users.noreply.github.com>
2025-08-03 19:11:44 +0530

    Add files via upload

--- service/BankService.java
+++ service/BankService.java
@@ -1,1 +1,27 @@
+package service;
 
+import model.BankAccount;
+
+public class BankService {
+    private BankAccount account;
+
+    public BankService(BankAccount account) {
+        this.account = account;
+    }
+
+    public void deposit(double amount) {
+        account.deposit(amount);
+    }
+
+    public void withdraw(double amount) {
+        account.withdraw(amount);
+    }
+
+    public void checkBalance() {
+        System.out.println("Your current balance is: ₹" + account.getBalance());
+    }
+
+    public void displayAccountInfo() {
+        account.showAccountInfo();
+    }
+}
